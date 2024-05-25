9ad05ae5ca21a725649452088b82cbb463f7beff
Jamie <jammieeeeeewhite@gmail.com>
2024-05-25 05:24:59 +0100

    No sudo

--- run.sh
+++ run.sh
@@ -1,12 +1,7 @@
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
-
-ifconfig wlan0 down;
-iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 3000`;
-dnsmasq -C ./dnsmasq.conf`;
-hostapd -B ./hostapd.conf`;
-ifconfig wlan0 up;
-ifconfig wlan0 192.168.1.1/3000;
+sudo ifconfig wlan0 down;
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 3000;
+sudo dnsmasq -C ./dnsmasq.conf;
+sudo hostapd -B ./hostapd.conf;
+sudo ifconfig wlan0 up;
+sudo ifconfig wlan0 192.168.1.1/3000;
 bun index.js;
